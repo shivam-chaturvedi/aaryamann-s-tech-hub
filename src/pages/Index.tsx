9177c0e3df5031d2e9152cbed7f822775b2ec5fd
Aranyo Ray <44411039+aranyoray@users.noreply.github.com>
2025-12-10 14:43:32 +0530

    Remove profile picture from cover photo carousel

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,7 +25,6 @@ const Index = () => {
   const heroImages = useMemo(() => [
     { src: "/projects/fsai/beetlebot_at_fsai-02.jpeg", caption: "BeetleBot - FSAI Conference" },
     { src: "/projects/compost/compost-bin-01.jpeg", caption: "Khadify - Sensor Deck" },
-    { src: "/projects/cyberdost-1.jpeg", caption: "CyberDost - Cyberbullying Advocacy" },
     { src: "/projects/fsai/beetlebot_at_fsai-05.jpeg", caption: "BeetleBot - FSAI Presentation" },
     { src: "/projects/compost/compost-bin-02.jpeg", caption: "Khadify - Aeration Setup" },
     { src: "/projects/cyberdost-07.jpeg", caption: "CyberDost - Community Outreach" },
@@ -36,7 +35,6 @@ const Index = () => {
     { src: "/projects/cyberdost-08.jpeg", caption: "CyberDost - Policy Engagement" },
     { src: "/projects/compost/compost-bin-11.jpeg", caption: "Khadify - Quality Analysis" },
     { src: "/projects/compost/compost-bin-12.jpeg", caption: "Khadify - Final Product" },
-    { src: "/projects/compost/compost-bin-11.jpeg", caption: "Khadify - Quality Analysis" },
   ], []);
   
   useEffect(() => {
